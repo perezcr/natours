a14a8e8c2910d87734d58eba311675e225e2bfd7
perezcr <crperz@outlook.com>
2020-06-06 17:46:10 -0500

    Set up environment variables.

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+const dotenv = require('dotenv');
+// It will read variables in file and save them into node.js environment variables
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
