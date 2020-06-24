7a7bd4bd9a5f6cc45f971ef815ee813299150994
perezcr <crperz@outlook.com>
2020-06-24 00:27:41 -0500

    Create User model.

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name'],
+    trim: true,
+    maxlength: [30, 'A user name must have less or equal then 30 characters'],
+  },
+  email: {
+    type: String,
+    required: [true, 'A user must have a email'],
+    trim: true,
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  role: {
+    type: String,
+    enum: ['user', 'guide', 'lead-guide', 'admin'],
+    default: 'user',
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: [8, 'Password must have more or equal then 8 characters'],
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on CREATE and SAVE!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!',
+    },
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
